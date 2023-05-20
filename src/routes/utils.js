2d84c7d579a30db5e7e94386b52f9e15d03f171f
MasterTemple <blake.scampone@gmail.com>
2023-05-19 22:07:24 -0700

    Mobile should now check user agent

--- src/routes/utils.js
+++ src/routes/utils.js
@@ -3,11 +3,28 @@ import { writable } from "svelte/store";
 export const device = writable('desktop');
 export const isMobile = writable(false);
 
+function detectMobile() {
+	const toMatch = [
+			/Android/i,
+			/webOS/i,
+			/iPhone/i,
+			/iPad/i,
+			/iPod/i,
+			/BlackBerry/i,
+			/Windows Phone/i
+	];
+	
+	return toMatch.some((toMatchItem) => {
+			return navigator.userAgent.match(toMatchItem);
+	});
+}
+
 export function checkDevice() {
-  console.log(window.innerWidth);
-  if (window.innerWidth < 1080) {
+  // console.log(window.innerWidth);
+  // if (window.innerWidth < 1080) {
   // if (window.innerWidth < 1080 && window.innerWidth < window.innerHeight) {
-    // if (window.innerWidth * 2 <= window.innerHeight) {
+	// if (window.innerWidth * 2 <= window.innerHeight) {
+	if (detectMobile) {
     device.set('mobile');
     isMobile.set(true);
   } else {
